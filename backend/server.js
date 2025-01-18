a38c67323e02e60cf43101ff5520431054639003
modembcc <modemhappy@gmail.com>
2025-01-18 17:27:02 +0800

    Add map and roomData

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,62 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
+const url = require("url");
 
 // Create a WebSocket server
 const server = new WebSocket.Server({ port: 8080 });
 
-server.on('connection', (socket) => {
-  console.log('Client connected');
+const rooms = new Map();
+
+class RoomData {
+  constructor() {
+    this.players = [];
+    this.started = false;
+    this.majorityWord = "";
+    this.minorityWord = "";
+    this.minorityPLayer = Math.floor(Math.random() * 4);
+    this.answers = [];
+    this.votes = [];
+  }
+
+  add(socket) {
+    this.players.push(socket);
+  }
+}
+
+server.on("connection", (socket, request) => {
+  const room = url.parse(request.url, true).query.room || "default";
+  console.log("Client connected");
+
+  if (!rooms.has(room)) {
+    rooms.set(room, new RoomData());
+  }
+  rooms.get(room).add(socket);
 
   // Send a message to the client
-  socket.send(JSON.stringify({ message: 'Welcome to the WebSocket server!' }));
+  socket.send(JSON.stringify({ message: "Welcome to the WebSocket server!" }));
 
   // Handle incoming messages
-  socket.on('message', (obj) => {
-    const parseddata = JSON.parse(obj)
-    console.log('Received from client:', parseddata.msg);
-    socket.send(JSON.stringify({ message: `You said: ${parseddata.msg}` }));
+  socket.on("message", (obj) => {
+    try {
+      const parseddata = JSON.parse(obj);
+      console.log("Received from client:", parseddata.msg);
+      BroadcastMsgToRoom(room, parseddata.msg);
+    } catch (e) {
+      socket.send(JSON.stringify(e));
+    }
   });
 
   // Handle client disconnection
-  socket.on('close', () => {
-    console.log('Client disconnected');
+  socket.on("close", () => {
+    console.log("Client disconnected");
   });
 });
+
+const BroadcastMsgToRoom = (room, msg) => {
+  if (rooms.has(room)) {
+    rooms.get(room).players.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
+  }
+};
